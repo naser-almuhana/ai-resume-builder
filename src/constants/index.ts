0d713504a8be80f328b6faf11657dab04b36a71d
naseralmuhana <naseralmuhana0@gmail.com>
2025-04-13 01:13:31 +0300

    initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "AI Resume Builder"
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job."
