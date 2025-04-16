6f1f04ad74e3f889dadfbe9ae439a232f0000fbe
naseralmuhana <naseralmuhana0@gmail.com>
2025-04-16 17:52:44 +0300

    home page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,6 +21,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        premium:
+          "bg-gradient-to-r from-green-600 to-green-400 text-white shadow hover:from-green-600/90 hover:to-green-400/90",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
